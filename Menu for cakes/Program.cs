b3e9899c4ee22fb55adb56ad67be9797cdb3744d
Izames <geday2@mail.ru>
2022-10-31 20:02:09 +0300

    Добавьте файлы проекта.

---
+++ Menu for cakes/Program.cs
@@ -0,0 +1,148 @@
+﻿
+using zakaz;
+
+namespace tortek
+{
+
+    internal class menuha
+    {
+
+        static void Main()
+        {
+            List<string> list = new List<string>() { "0", "", "0", "" };
+            menuha adres = new menuha();
+            adres.Man(list);
+        }
+        public void Man(List<string> spisok)
+        {
+
+            int sum = Convert.ToInt32(spisok[0]) + Convert.ToInt32(spisok[2]);
+            string things = spisok[1] + spisok[3];
+            spisok[3] = things;
+            spisok[2] = Convert.ToString(sum);
+            punkti vibor = new punkti();
+            int pozition = 1;
+            string path = "C:\\Users\\geday\\OneDrive\\Рабочий стол\\Чеки.txt";
+            if (File.Exists(path) == false)
+            {
+                File.Create(path);
+            }
+
+
+            string[] cheki = File.ReadAllLines(path);
+
+            int ch = cheki.Length;
+
+            string[] cheki2 = new string[cheki.Length + 1];
+            int ch2 = cheki2.Length;
+            for (int i = 0; i < ch;)
+            {
+                cheki2[ch2 - 1] = cheki[ch - 1];
+                ch2--; ch--;
+            }
+
+
+            Console.WriteLine("для своего торта вы можете выбрать форму, декорации, размер, начинку, глазурь и количество коржей");
+            Console.WriteLine("\tформа торта\n\tдекорации для торта\n\tразмер торта\n\tначинка торта\n\tглазурь для торта\n\tколичество коржей для торта\n\tоформить заказ\nобщая стоимость: " + sum + " Эдди \nингридиенты: " + things);
+            Console.SetCursorPosition(0, pozition);
+            Console.WriteLine("-->");
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            Console.Clear();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+
+                if (Clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    pozition--;
+                }
+                if (Clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    pozition++;
+                }
+                if (pozition > 7)
+                {
+                    pozition = 1;
+                }
+                if (pozition < 1)
+                {
+                    pozition = 7;
+                }
+                Console.WriteLine("для своего торта вы можете выбрать форму, декорации, размер, начинку, глазурь и количество коржей");
+                Console.WriteLine("\tформа торта\n\tдекорации для торта\n\tразмер торта\n\tначинка торта\n\tглазурь для торта\n\tколичество коржей для торта\n\tоформить заказ\nобщая стоимость: " + sum + " Эдди\nингридиенты: " + things);
+                Console.SetCursorPosition(0, pozition);
+                Console.WriteLine("-->");
+                Clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+            Convert.ToString(spisok[0]);
+            Convert.ToString(spisok[2]);
+            if (pozition == 1)
+            {
+                vibor.form(spisok);
+            }
+            if (pozition == 2)
+            {
+                vibor.decoration(spisok);
+            }
+            if (pozition == 3)
+            {
+                vibor.size(spisok);
+            }
+            if (pozition == 4)
+            {
+                vibor.vkus(spisok);
+            }
+            if (pozition == 5)
+            {
+                vibor.glasuur(spisok);
+            }
+            if (pozition == 6)
+            {
+                vibor.quntity(spisok);
+            }
+            if (pozition == 7)
+            {
+
+                cheki2[0] = DateTime.Now + "\nОбщая стоимость:" + spisok[2] + " эдди. \n\t\tВсе Ингредиенты:" + things;
+                File.WriteAllLines(path, cheki2);
+                int cursor = 1;
+                Console.WriteLine("Ваш заказ оформлен, спасибо, что выбрали нас. Хотите оформить еще один заказ?\n\tда\n\tнет");
+                Console.SetCursorPosition(0, cursor);
+                Console.WriteLine("-->");
+                ConsoleKeyInfo Clavishu = Console.ReadKey();
+                Console.Clear();
+                while (Clavishu.Key != ConsoleKey.Enter)
+                {
+
+                    if (Clavishu.Key == ConsoleKey.UpArrow)
+                    {
+                        cursor--;
+                    }
+                    if (Clavishu.Key == ConsoleKey.DownArrow)
+                    {
+                        cursor++;
+                    }
+                    if (cursor > 2)
+                    {
+                        cursor = 1;
+                    }
+                    if (cursor < 1)
+                    {
+                        cursor = 2;
+                    }
+                    Console.WriteLine("Ваш заказ оформлен, спасибо, что выбрали нас. Хотите оформить еще один заказ?\n\tда\n\tнет");
+                    Console.SetCursorPosition(0, cursor);
+                    Console.WriteLine("-->");
+                    Clavishu = Console.ReadKey();
+                    Console.Clear();
+                }
+                if (cursor == 1)
+                {
+                    List<string> list = new List<string>() { "0", "", "0", "" };
+                    menuha adres = new menuha();
+                    adres.Man(list);
+                }
+            }
+        }
+    }
+}
---
+++ Menu for cakes/choosing.cs
@@ -0,0 +1,322 @@
+﻿using tortek;
+
+namespace zakaz
+{
+    internal class punkti
+    {
+        public void form(List<string> forms)
+        {
+
+            int pozition = 1;
+            Console.WriteLine("форма коржей:");
+            Console.WriteLine("\tквадрат: 100 Эдди\n\tпрямоугольник: 100 Эдди\n\tпирамидка: 200 Эдди");
+            Console.SetCursorPosition(0, pozition);
+            Console.WriteLine("-->");
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            Console.Clear();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+                if (Clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    pozition--;
+                }
+                if (Clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    pozition++;
+                }
+                if (pozition > 3)
+                {
+                    pozition = 1;
+                }
+                if (pozition < 1)
+                {
+                    pozition = 3;
+                }
+                Console.WriteLine("форма коржей:");
+                Console.WriteLine("\tквадрат: 100 Эдди\n\tпрямоугольник: 100 Эдди\n\tпирамидка: 200 Эдди");
+                Console.SetCursorPosition(0, pozition);
+                Console.WriteLine("-->");
+                Clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+            if (pozition == 1)
+            {
+                forms[0] = "100";
+                forms[1] = "квадрат: 100 Эдди; ";
+            }
+            if (pozition == 2)
+            {
+                forms[0] = "100";
+                forms[1] = "прямоугольник: 100 Эдди; ";
+            }
+            if (pozition == 3)
+            {
+                forms[0] = "200";
+                forms[1] = "пирамидка: 200 Эдди; ";
+            }
+            menuha back = new menuha();
+            back.Man(forms);
+        }
+        public void size(List<string> forms)
+        {
+            int pozition = 1;
+            Console.WriteLine("размер:");
+            Console.WriteLine("\t10 сантиметров: 100 Эдди\n\t20 сантиметров: 200 Эдди\n\t30 сантиметров: 300 Эдди");
+            Console.SetCursorPosition(0, pozition);
+            Console.WriteLine("-->");
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            Console.Clear();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+                if (Clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    pozition--;
+                }
+                if (Clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    pozition++;
+                }
+                if (pozition > 3)
+                {
+                    pozition = 1;
+                }
+                if (pozition < 1)
+                {
+                    pozition = 3;
+                }
+                Console.WriteLine("размер:");
+                Console.WriteLine("\t10 сантиметров: 100 Эдди\n\t20 сантиметров: 200 Эдди\n\t30 сантиметров: 300 Эдди");
+                Console.SetCursorPosition(0, pozition);
+                Console.WriteLine("-->");
+                Clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+            if (pozition == 1)
+            {
+                forms[0] = "100";
+                forms[1] = "10 сантиметров: 100 Эдди; ";
+            }
+            if (pozition == 2)
+            {
+                forms[0] = "200";
+                forms[1] = "20 сантиметров: 200 Ждди; ";
+            }
+            if (pozition == 3)
+            {
+                forms[0] = "300";
+                forms[1] = "30 сантиметров: 300 Эдди; ";
+            }
+            menuha back = new menuha();
+            back.Man(forms);
+        }
+        public void decoration(List<string> forms)
+        {
+            int pozition = 1;
+            Console.WriteLine("декорации:");
+            Console.WriteLine("\tфото именинника 100 Эдди\n\tколизей 200 Эдди\n\tуголь 300 Эдди");
+            Console.SetCursorPosition(0, pozition);
+            Console.WriteLine("-->");
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            Console.Clear();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+                if (Clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    pozition--;
+                }
+                if (Clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    pozition++;
+                }
+                if (pozition > 3)
+                {
+                    pozition = 1;
+                }
+                if (pozition < 1)
+                {
+                    pozition = 3;
+                }
+                Console.WriteLine("декорации:");
+                Console.WriteLine("\tфото именинника 100 Эдди\n\tколизей 200 Эдди\n\tуголь 300 Эдди");
+                Console.SetCursorPosition(0, pozition);
+                Console.WriteLine("-->");
+                Clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+
+            if (pozition == 1)
+            {
+                forms[0] = "100";
+                forms[1] = "фото именинника 100 Эдди; ";
+            }
+            if (pozition == 2)
+            {
+                forms[0] = "200";
+                forms[1] = "колизей 200 Эдди; ";
+            }
+            if (pozition == 3)
+            {
+                forms[0] = "300";
+                forms[1] = "уголь 300 Эдди; ";
+            }
+            menuha back = new menuha();
+            back.Man(forms);
+        }
+        public void vkus(List<string> forms)
+        {
+            int pozition = 1;
+            Console.WriteLine("начинка:");
+            Console.WriteLine("\tначинка киви: 100 Эдди\n\tначинка сливки: 200 Эдди\n\tначинка памело: 300 Эдди");
+            Console.SetCursorPosition(0, pozition);
+            Console.WriteLine("-->");
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            Console.Clear();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+                if (Clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    pozition--;
+                }
+                if (Clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    pozition++;
+                }
+                if (pozition > 3)
+                {
+                    pozition = 1;
+                }
+                if (pozition < 1)
+                {
+                    pozition = 3;
+                }
+                Console.WriteLine("начинка:");
+                Console.WriteLine("\tначинка киви: 100 Эдди\n\tначинка сливки: 200 Эдди\n\tначинка памело: 300 Эдди");
+                Console.SetCursorPosition(0, pozition);
+                Console.WriteLine("-->");
+                Clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+            if (pozition == 1)
+            {
+                forms[0] = "100";
+                forms[1] = "начинка киви: 100 Эдди; ";
+            }
+            if (pozition == 2)
+            {
+                forms[0] = "200";
+                forms[1] = "начинка сливки: 200 Эдди; ";
+            }
+            if (pozition == 3)
+            {
+                forms[0] = "300";
+                forms[1] = "начинка памело: 300 Эдди; ";
+            }
+            menuha back = new menuha();
+            back.Man(forms);
+        }
+        public void quntity(List<string> forms)
+        {
+            int pozition = 1;
+            Console.WriteLine("количество коржей:");
+            Console.WriteLine("\tодин корж: 100 Эдди\n\tдва коржа: 200 Эдди\n\tтри коржа: 300 Эдди");
+            Console.SetCursorPosition(0, pozition);
+            Console.WriteLine("-->");
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            Console.Clear();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+                if (Clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    pozition--;
+                }
+                if (Clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    pozition++;
+                }
+                if (pozition > 3)
+                {
+                    pozition = 1;
+                }
+                if (pozition < 1)
+                {
+                    pozition = 3;
+                }
+                Console.WriteLine("количество коржей:");
+                Console.WriteLine("\tодин корж: 100 Эдди\n\tдва коржа: 200 Эдди\n\tтри коржа: 300 Эдди");
+                Console.SetCursorPosition(0, pozition);
+                Console.WriteLine("-->");
+                Clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+            if (pozition == 1)
+            {
+                forms[0] = "100";
+                forms[1] = "один корж: 100 Эдди; ";
+            }
+            if (pozition == 2)
+            {
+                forms[0] = "200";
+                forms[1] = "два коржа: 200 Эдди; ";
+            }
+            if (pozition == 3)
+            {
+                forms[0] = "300";
+                forms[1] = "три коржа: 300 Эдди; ";
+            }
+            menuha back = new menuha();
+            back.Man(forms);
+        }
+        public void glasuur(List<string> forms)
+        {
+            int pozition = 1;
+            Console.WriteLine("глазурь:");
+            Console.WriteLine("\tклубничная глазурь: 100 Эдди\n\tшоколадная глазурь: 200 Эдди\n\tкарамельная глазурь: 300 Эдди");
+            Console.SetCursorPosition(0, pozition);
+            Console.WriteLine("-->");
+            ConsoleKeyInfo Clavisha = Console.ReadKey();
+            Console.Clear();
+            while (Clavisha.Key != ConsoleKey.Enter)
+            {
+                if (Clavisha.Key == ConsoleKey.UpArrow)
+                {
+                    pozition--;
+                }
+                if (Clavisha.Key == ConsoleKey.DownArrow)
+                {
+                    pozition++;
+                }
+                if (pozition > 3)
+                {
+                    pozition = 1;
+                }
+                if (pozition < 1)
+                {
+                    pozition = 3;
+                }
+                Console.WriteLine("глазурь:");
+                Console.WriteLine("\tклубничная глазурь: 100 Эдди\n\tшоколадная глазурь: 200 Эдди\n\tкарамельная глазурь: 300 Эдди");
+                Console.SetCursorPosition(0, pozition);
+                Console.WriteLine("-->");
+                Clavisha = Console.ReadKey();
+                Console.Clear();
+            }
+            if (pozition == 1)
+            {
+                forms[0] = "100";
+                forms[1] = "клубничная глазурь: 100 Эдди; ";
+            }
+            if (pozition == 2)
+            {
+                forms[0] = "200";
+                forms[1] = "шоколадная глазурь: 200 Эдди; ";
+            }
+            if (pozition == 3)
+            {
+                forms[0] = "300";
+                forms[1] = "карамельная глазурь: 300 Эдди; ";
+            }
+            menuha back = new menuha();
+            back.Man(forms);
+        }
+    }
+}
